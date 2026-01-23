35074565c3528d7ec00789167e1a227d21a14ee3
sonukumarkushvaha1-hub <sonukumarkushvaha1@gmail.com>
2026-01-23 22:19:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,8 @@ DB_NAME = os.environ.get("DB_NAME", "Cluster0")
 
 
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "1003626737474"))
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1003626737474"))
+FORCE_SUB_LINK = os.environ.get("FORCE_SUB_LINK", "https://t.me/+3TdvTuzF0gtkNmRl")
 
 
 FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "600")) # auto delete in seconds
