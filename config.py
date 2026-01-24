a68a9e58dfbe93a2f0c8eba3fca3a918226639d0
sonukumarkushvaha1-hub <sonukumarkushvaha1@gmail.com>
2026-01-25 00:10:26 +0530

    Change BOT_TOKEN in config.py
    Updated BOT_TOKEN to a new value.

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from logging.handlers import RotatingFileHandler
 
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8583460812:AAHFRwRfa8OUXbwuDb484KwXkYDXIDSlJew")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8583460812:AAEyTMW-JYP3_JIQSGsh3thPhMAoh2ZxArY")
 API_ID = int(os.environ.get("API_ID", "32460404"))
 API_HASH = os.environ.get("API_HASH", "410d526521ae7dcc474e0f4246788560")
 
