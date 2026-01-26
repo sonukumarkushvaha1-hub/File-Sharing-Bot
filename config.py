fc5f903118423fbd3084463ab22a0cf569267043
sonukumarkushvaha1-hub <sonukumarkushvaha1@gmail.com>
2026-01-26 13:59:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,10 +12,10 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "5569039254"))
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://sonukumarkushvaha1_db_user:6w9MVqKN8wjNlqqS@cluster0.2eu5lot.mongodb.net/?appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "Cluster0")
 
-# --- CHANNELS (Force Sub Removed) ---
+# --- CHANNELS (Set to None if not using) ---
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003626737474"))
-FORCE_SUB_CHANNEL = None # यहाँ से चैनल हटा दिया गया है
-FORCE_SUB_LINK = None    # लिंक भी हटा दिया गया है
+FORCE_SUB_CHANNEL = None 
+FORCE_SUB_LINK = os.environ.get("FORCE_SUB_LINK", "")
 
 # --- SETTINGS ---
 FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "600"))
@@ -37,9 +37,11 @@ CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 PROTECT_CONTENT = os.environ.get('PROTECT_CONTENT', "False").lower() == "true"
 DISABLE_CHANNEL_BUTTON = os.environ.get('DISABLE_CHANNEL_BUTTON', "True").lower() == "true"
 
-# --- MESSAGES ---
+# --- MESSAGES (Import Errors को रोकने के लिए ये सब यहाँ होने चाहिए) ---
 START_MSG = os.environ.get("START_MESSAGE", "Hello {mention}\n\nI Can Store Private Files In Specified Channel.")
-# FORCE_MSG की अब ज़रूरत नहीं है इसलिए इसे हटा सकते हैं
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {mention}\n\n<b>You Need To Join My Channel To Use Me.</b>") # यह लाइन वापस जोड़ दी गई है
+BOT_STATS_TEXT = "<b>BOT UPTIME :</b>\n{uptime}"
+USER_REPLY_TEXT = "❌Don't Send Me Messages Directly I'm Only File Share Bot !"
 
 # --- LOGGING ---
 LOG_FILE_NAME = "filesharingbot.txt"
